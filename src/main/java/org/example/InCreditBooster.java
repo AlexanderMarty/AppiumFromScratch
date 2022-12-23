65081c4b9cf1ed78253b28575972f72fe610c628
Alexandr Martynenko <omartynenko@credolab.com>
2022-12-23 15:43:11 +0200

    Assert was learned

--- src/main/java/org/example/InCreditBooster.java
+++ src/main/java/org/example/InCreditBooster.java
@@ -1,23 +1,26 @@
+package org.example;
+
+import dev.failsafe.internal.util.Assert;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-//setting capabilities via code
-
-public class AutomationAppium {
+public class InCreditBooster {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
-        desiredCapabilities.setCapability("platformName", "Android");
-        desiredCapabilities.setCapability("noReset", true);
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Device");
+        desiredCapabilities.setCapability(MobileCapabilityType.NO_RESET, false);
 
         URL url = new URL("http://127.0.0.1:4808/wd/hub");
-       AndroidDriver driver = new AndroidDriver(url, desiredCapabilities);
+        AndroidDriver driver = new AndroidDriver(url, desiredCapabilities);
 
         var el1 =  driver.findElement(By.xpath("//android.widget.ImageView[@content-desc=\"IN Credit Booster\"]"));
         el1.click();
@@ -29,7 +32,7 @@ public class AutomationAppium {
 //        Thread.sleep(2000);
         var el3 = driver.findElement(By.id(packageName + ":id/onboarding_skip"));
         el3.click();
-      Thread.sleep(2000);
+        Thread.sleep(2000);
         var el4 = driver.findElement(By.id(packageName + ":id/membership_id_field"));
         el4.sendKeys("qwe");
         Thread.sleep(2000);
@@ -40,18 +43,10 @@ public class AutomationAppium {
         el6.click();
         Thread.sleep(2000);
 
-//        MobileElement el1 = (MobileElement) driver.findElementByXPath("//android.widget.ImageView[@content-desc=\"IN Credit Booster\"]");
-//        el1.click();
-//        MobileElement el2 = (MobileElement) driver.findElementById("incr.credoapp:id/onboarding_next");
-//        el2.click();
-//        MobileElement el3 = (MobileElement) driver.findElementById("incr.credoapp:id/onboarding_skip");
-//        el3.click();
-//        MobileElement el4 = (MobileElement) driver.findElementById("incr.credoapp:id/membership_id_field");
-//        el4.sendKeys("qwe");
-//        MobileElement el5 = (MobileElement) driver.findElementById("incr.credoapp:id/loancalculation_agreement_check_result");
-//        el5.click();
-//        MobileElement el6 = (MobileElement) driver.findElementById("incr.credoapp:id/application_form_continue_btn");
-//        el6.click();
 
+        WebElement el7 = driver.findElement(By.id(packageName+ ":id/appCompatTextView"));
+        Assert.isTrue(el7.getText().contains("Allow mobile permissions to generate credit score"), "Alarm");
+
+//        Assert.isTrue(el7.getText().startsWith("Allow"), "Alarm");
     }
 }
